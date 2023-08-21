e302e7e0f6f9ea0e2815bebb2ec04236877ccf61
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-08-21 14:22:06 +0200

    feat: exporting the %.% operator

--- R/0_R6_utils.R
+++ R/0_R6_utils.R
@@ -45,3 +45,22 @@ diseasyoption <- function(option, self) {
     purrr::discard(~ is.character(.) && . == "") |>
     purrr::pluck(1)
 }
+
+
+#' Existence aware pick operator
+#' @param env (`object`)\cr
+#'   The object or environment to attempt to pick from
+#' @param field (`character`)\cr
+#'   The name of the field to pick from `env`
+#' @return
+#'   Error if the `field` does not exist in `env`, otherwise it returns `field`
+#' @export
+`%.%` <- function(env, field) {
+  field_name <- as.character(substitute(field))
+  env_name <- as.character(substitute(env))
+  if (is.environment(env)) env <- as.list(env, all.names = TRUE)
+  if (!(field_name %in% names(env))) {
+    stop(field_name, " not found in ", env_name)
+  }
+  return(purrr::pluck(env, field_name))
+}
