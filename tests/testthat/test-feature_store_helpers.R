a065e7cbe597b35427373282d1e90d4d4bc8a226
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-08-29 11:03:47 +0200

    feat: allow character input to diseasyoption
    fixes Improve diseasyoption #23

--- tests/testthat/test-feature_store_helpers.R
+++ tests/testthat/test-feature_store_helpers.R
@@ -64,30 +64,3 @@ test_that("get_diseasystore works", {
   expect_identical(get_diseasystore("Google COVID 19")$classname, "DiseasystoreGoogleCovid19")
 
 })
-
-
-test_that("%.% works", {
-
-  d <- list(a = 2, b = 3, .c = 4)
-
-  expect_identical(d %.% a, 2)
-  expect_identical(d %.% b, 3)
-  expect_identical(d %.% .c, 4)
-  expect_error(d %.% d, "d not found in d")
-
-
-  d <- c(a = 2, b = 3, .c = 4)
-
-  expect_identical(d %.% a, 2)
-  expect_identical(d %.% b, 3)
-  expect_identical(d %.% .c, 4)
-  expect_error(d %.% d, "d not found in d")
-
-
-  d <- R6::R6Class(public = list(a = 2, b = 3, .c = 4))$new()
-
-  expect_identical(d %.% a, 2)
-  expect_identical(d %.% b, 3)
-  expect_identical(d %.% .c, 4)
-  expect_error(d %.% d, "d not found in d")
-})
