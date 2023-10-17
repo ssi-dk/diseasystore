ad70f48a38bbe5e5beae19bf16cf3c645aa80ecf
Rasmus Skytte Randløv <rske@ssi.dk>
2023-10-17 12:59:31 +0200

    test(source_conn_path): add tests

--- tests/testthat/test-feature_store_helpers.R
+++ tests/testthat/test-feature_store_helpers.R
@@ -64,3 +64,85 @@ test_that("get_diseasystore works", {
   expect_identical(get_diseasystore("Google COVID 19")$classname, "DiseasystoreGoogleCovid19")
 
 })
+
+
+test_that("source_conn_path works for directories", {
+
+  # Create some files to test for
+  test_dir1 <- tempdir()
+  expect_true(dir.exists(test_dir1))
+
+  test_file1 <- basename(tempfile())
+  saveRDS(mtcars, file = file.path(test_dir1, test_file1))
+
+  test_file2 <- paste0(test_file1, ".gz")
+  saveRDS(mtcars, file = file.path(test_dir1, test_file2))
+
+  test_file3 <- basename(tempfile())
+  saveRDS(mtcars, file = file.path(test_dir1, paste0(test_file3, ".gz")))
+
+  test_file4 <- basename(tempfile())
+
+  # test_file1 and test_file2 exists
+  expect_true(file.exists(file.path(test_dir1, test_file1)))
+  expect_true(file.exists(file.path(test_dir1, test_file2)))
+
+  # .. and they give paths as expected
+  expect_identical(source_conn_path(test_dir1, test_file1),
+                   file.path(test_dir1, test_file1))
+  expect_identical(source_conn_path(test_dir1, test_file2),
+                   file.path(test_dir1, test_file2))
+
+
+  # test_file3 and test_file4 does not exists
+  expect_false(file.exists(file.path(test_dir1, test_file3))) # does not exist
+  expect_false(file.exists(file.path(test_dir1, test_file4))) # does not exist
+
+  # test_file4 give errors as expected
+  expect_error(source_conn_path(test_dir1, test_file4),
+               class = "simpleError",
+               regexp = "could not be found in")
+
+  # but a test_file3.gz exists so we do get a path
+  expect_true(file.exists(file.path(test_dir1, paste0(test_file3, ".gz"))))
+  expect_identical(source_conn_path(test_dir1, test_file3),
+                   file.path(test_dir1, paste0(test_file3, ".gz")))
+
+
+
+
+  # If the files are not in the folder, all gives errors
+  test_dir2 <- stringr::str_replace(test_dir1, basename(test_dir1), tempfile(tmpdir = ""))
+
+  expect_error(source_conn_path(test_dir2, test_file1),
+               class = "simpleError",
+               regexp = "check_directory_exists")
+  expect_error(source_conn_path(test_dir2, test_file2),
+               class = "simpleError",
+               regexp = "check_directory_exists")
+  expect_error(source_conn_path(test_dir2, test_file3),
+               class = "simpleError",
+               regexp = "check_directory_exists")
+  expect_error(source_conn_path(test_dir2, test_file4),
+               class = "simpleError",
+               regexp = "check_directory_exists")
+})
+
+
+test_that("source_conn_path works for URLs", {
+
+  # Create some files to test for
+  test_url1 <- "https://test.com/"
+  test_url2 <- "https://test.com"
+
+  test_file <- basename(tempfile())
+
+
+  # Test the different combinations
+  expect_character(source_conn_path(test_url1, test_file),
+                   pattern = paste0(stringr::str_remove(test_url1, "/$"), "/", test_file))
+
+  expect_character(source_conn_path(test_url2, test_file),
+                   pattern = paste0(stringr::str_remove(test_url2, "/$"), "/", test_file))
+
+})
