d5037d57f2cb117f3af0dbfb491c98af2a54d8eb
Marcus Munch Grünewald <MMGR@SSI.dk>
2023-11-13 10:27:54 +0100

    tests(test-0_*): find directories with system.file

--- tests/testthat/test-0_examples.R
+++ tests/testthat/test-0_examples.R
@@ -1,23 +1,8 @@
 test_that(r"{.Rd files have \examples}", {
 
-  # Load .Rd files based on environment
-  # When using R-CMD-Check the deployment is different from when using devtools::test().
-  # Deployment on Github is also different from running R-CMD-Check locally
-  # Here we need to read directly from a .rdx database stored in the "help" folder
-  # If we are testing locally, we read from the "man" folder
-  # Note that `devtools::test()`, `devtools::check()` and GitHub workflows all have different
-  # folder structures during testing, so we need to account for these differences
-
-  # Platform independent regex search to find root folder
-  path_regex <- stringr::str_replace(r"{(.*\/\w*(.Rcheck)?)\/.*(?=\/testthat)}", "/", .Platform$file.sep)
-  pkg_path <- stringr::str_extract(getwd(), path_regex, group = 1)
-
-  # If path contains .Rcheck, we need to add the package name to the path
-  pkg_path <- stringr::str_replace(pkg_path, r"{(\w*)(\.Rcheck)}", paste0("\\1\\2", .Platform$file.sep, "\\1"))
-
   # Look for the source of .Rd files
-  help_dir <- file.path(pkg_path, "help")
-  man_dir  <- file.path(pkg_path, "man")
+  help_dir <- system.file("help", package = testthat::testing_package())
+  man_dir <- system.file("man", package = testthat::testing_package())
 
   if (checkmate::test_directory_exists(help_dir)) {
 
--- tests/testthat/test-0_non_ascii.R
+++ tests/testthat/test-0_non_ascii.R
@@ -1,23 +1,11 @@
 test_that("Code contains no non-ASCII characters", {
 
-  # Load .Rd files based on environment
-  # When using R-CMD-Check the deployment is different from when using devtools::test().
-  # Deployment on Github is also different from running R-CMD-Check locally
-  # Here we need to read directly from a .rdx database stored in the "help" folder
-  # If we are testing locally, we read from the "man" folder
-  # Note that `devtools::test()`, `devtools::check()` and GitHub workflows all have different
-  # folder structures during testing, so we need to account for these differences
-
-  # Platform independent regex search to find root folder
-  path_regex <- stringr::str_replace(r"{(.*\/\w*(.Rcheck)?)\/.*(?=\/testthat)}", "/", .Platform$file.sep)
-  pkg_path <- stringr::str_extract(getwd(), path_regex, group = 1)
-
-  # If path contains .Rcheck, we need to add the package name to the path
-  pkg_path <- stringr::str_replace(pkg_path, r"{(\w*)(\.Rcheck)}", paste0("\\1\\2", .Platform$file.sep, "\\1"))
-
   # Look for the source of .Rd files
-  help_dir <- file.path(pkg_path, "help")
-  man_dir  <- file.path(pkg_path, "man")
+  help_dir <- system.file("help", package = testthat::testing_package())
+  man_dir <- system.file("man", package = testthat::testing_package())
+
+  # Get path to R folder
+  r_dir <- system.file("R", package = testthat::testing_package())
 
   if (checkmate::test_directory_exists(help_dir)) {
 
@@ -42,8 +30,6 @@ test_that("Code contains no non-ASCII characters", {
 
   # Skip the "*-package.Rd" file
   rd_files <- rd_files[!stringr::str_detect(names(rd_files), "-package.[Rr][Dd]$")]
-
-  r_dir <- file.path(pkg_path, "R")
 
   if (checkmate::test_directory_exists(r_dir)) {
 
