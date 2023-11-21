2392d9b25bd41c79ae449ceea697a23c7efc5146
Rasmus Skytte Randløv <rske@ssi.dk>
2023-11-21 08:42:09 +0100

    test(SCDB-messages): add missing target_schema

--- tests/testthat/test-SCDB_messages.R
+++ tests/testthat/test-SCDB_messages.R
@@ -1,6 +1,8 @@
 test_that("SCDB gives too many messages", {
   conn <- DBI::dbConnect(RSQLite::SQLite())
 
+  target_schema <- "test_ds"
+
   test_id <- SCDB::id(paste(target_schema, "mtcars", sep = "."), conn)
   DBI::dbWriteTable(conn, test_id, mtcars)
   expect_message(SCDB::get_table(conn, test_id), "If you want to specify a schema use")
