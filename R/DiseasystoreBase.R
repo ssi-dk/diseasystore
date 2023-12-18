3c5a86f90cff83b5b9cb483e890cd5e7761d8dd4
Rasmus Skytte Randløv <rske@ssi.dk>
2023-12-18 10:58:18 +0100

    chore(DiseasystoreBase): remove stray print statements

--- R/DiseasystoreBase.R
+++ R/DiseasystoreBase.R
@@ -224,18 +224,6 @@ DiseasystoreBase <- R6::R6Class(                                                
             log_conn = self %.% target_conn
           ))
 
-          if (dbplyr::remote_name(logger$log_tbl) == "logs") {
-            print("log_table_id")
-            print(log_table_id)
-            print("logger$log_conn")
-            print(logger$log_conn)
-            print("logger$log_tbl")
-            print(logger$log_tbl)
-            print("print(dbplyr::remote_name(logger$log_tbl))")
-            print(dbplyr::remote_name(logger$log_tbl))
-            stop()
-          }
-
           # Commit to DB
           suppressMessages(SCDB::update_snapshot(
             .data = ds_updated_feature,
