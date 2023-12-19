70b209477ab21331051df933ade2d2075126f18c
Rasmus Skytte Randløv <rske@ssi.dk>
2023-12-19 10:35:08 +0100

    test: test we do not write new files

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -5,3 +5,9 @@ test_that(r"{options are conserved during testing}", {
 
   expect_identical(current_diseasy_opts, diseasy_opts)
 })
+
+
+test_that(r"{data is not written locally}", {
+  # CRAN does not allow us to create new files in the package folder
+  expect_setequal(dir(recursive = TRUE), current_files)
+})
