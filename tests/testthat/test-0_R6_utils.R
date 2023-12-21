73d4b5a6db8979fb539cd21a97ea439f7522e8c0
RasmusSkytte <rasmusskytteeriksen@gmail.com>
2023-12-21 10:52:53 +0000

    chore: synced local 'tests/testthat/test-0_R6_utils.R' with remote 'tests/testthat/test-0_R6_utils.R'

--- tests/testthat/test-0_R6_utils.R
+++ tests/testthat/test-0_R6_utils.R
@@ -1,60 +1,29 @@
-test_that("printr works", {
-
-  # Check that printr works without file printing
-  # 1)
-  checkmate::expect_character(capture.output(printr("test string")),               pattern = r"{test string}")
-  # 2)
-  checkmate::expect_character(capture.output(printr("test1", "test2")),            pattern = r"{test1test2}")
-  # 3)
-  checkmate::expect_character(capture.output(printr("test1", "test2", sep = " ")), pattern = r"{test1 test2}")
-
-  # Check that printr works with file printing
-  test_file <- tempfile()
-
-  # 1)
-  checkmate::expect_character(capture.output(printr("test string",    file = test_file)),
-                              pattern = r"{test string}")
-  checkmate::expect_character(readLines(test_file),
-                              pattern = r"{test string}")
-  file.remove(test_file)
-
-
-  # 2)
-  checkmate::expect_character(capture.output(printr("test1", "test2", file = test_file)),
-                              pattern = r"{test1test2}")
-  checkmate::expect_character(readLines(test_file),
-                              pattern = r"{test1test2}")
-  file.remove(test_file)
-
-
-  # 3)
-  checkmate::expect_character(capture.output(printr("test1", "test2", file = test_file, sep = " ")),
-                              pattern = r"{test1 test2}")
-  checkmate::expect_character(readLines(test_file),
-                              pattern = r"{test1 test2}")
-  file.remove(test_file)
-})
-
-
 test_that("diseasyoption works", {
 
-  # Check that diseasyoption works for default values
-  expect_null(diseasyoption("non_existent_option"))
+  # Store the current options
+  opts <- options(
+    "diseasystore.target_schema" = "",
+    "diseasystore.DiseasystoreGoogleCovid19.target_schema" = ""
+  )
 
-  withr::local_options("diseasystore.target_schema" = target_schema_1)
-  expect_identical(diseasyoption("target_schema"), target_schema_1)
+  # Check that diseasyoption works for default values
+  expect_equal(diseasyoption("target_schema"), NULL)
+
+  options("diseasystore.target_schema" = "target_schema_1")
+  expect_equal(diseasyoption("target_schema"), "target_schema_1")
 
   # Check that it works for child classes
   ds <- DiseasystoreGoogleCovid19$new(target_conn = DBI::dbConnect(RSQLite::SQLite()))
-  expect_identical(diseasyoption("target_schema", "DiseasystoreGoogleCovid19"), target_schema_1)
-  expect_identical(diseasyoption("target_schema", ds), target_schema_1)
+  expect_equal(diseasyoption("target_schema", "DiseasystoreGoogleCovid19"), "target_schema_1")
+  expect_equal(diseasyoption("target_schema", ds), "target_schema_1")
 
-  withr::local_options("diseasystore.DiseasystoreGoogleCovid19.target_schema" = target_schema_2)
-  expect_identical(diseasyoption("target_schema", "DiseasystoreGoogleCovid19"), target_schema_2)
-  expect_identical(diseasyoption("target_schema", ds), target_schema_2)
+  options("diseasystore.DiseasystoreGoogleCovid19.target_schema" = "target_schema_2")
+  expect_equal(diseasyoption("target_schema", "DiseasystoreGoogleCovid19"), "target_schema_2")
+  expect_equal(diseasyoption("target_schema", ds), "target_schema_2")
 
-  rm(ds)
-  invisible(gc())
+  # Reset options
+  options(opts)
+  rm(opts)
 })
 
 
@@ -98,7 +67,7 @@ test_that("parse_diseasyconn works", {
   null_conn <- NULL
 
   # Test inputs for source_conn
-  conn <- expect_no_condition(parse_diseasyconn(valid_function_conn, type = "source_conn"))
+  expect_no_condition(conn <- parse_diseasyconn(valid_function_conn, type = "source_conn"))
   checkmate::expect_class(conn, "DBIConnection")
   DBI::dbDisconnect(conn)
 
@@ -107,21 +76,19 @@ test_that("parse_diseasyconn works", {
                class = "simpleError", regexp = "`conn` could not be parsed!")
 
 
-  conn <- expect_no_condition(parse_diseasyconn(valid_dbi_conn, type = "source_conn"))
+  expect_no_condition(conn <- parse_diseasyconn(valid_dbi_conn, type = "source_conn"))
   expect_identical(conn, valid_dbi_conn)
-  checkmate::expect_class(conn, "DBIConnection")
 
-
-  conn <- expect_no_condition(parse_diseasyconn(valid_str_conn, type = "source_conn"))
+  expect_no_condition(conn <- parse_diseasyconn(valid_str_conn, type = "source_conn"))
   expect_identical(conn, valid_str_conn)
 
 
-  conn <- expect_no_condition(parse_diseasyconn(null_conn, type = "source_conn"))
+  expect_no_condition(conn <- parse_diseasyconn(null_conn, type = "source_conn"))
   expect_null(conn)
 
 
   # Test inputs for target_conn
-  conn <- expect_no_condition(parse_diseasyconn(valid_function_conn, type = "target_conn"))
+  expect_no_condition(conn <- parse_diseasyconn(valid_function_conn, type = "target_conn"))
   checkmate::expect_class(conn, "DBIConnection")
   DBI::dbDisconnect(conn)
 
@@ -130,18 +97,14 @@ test_that("parse_diseasyconn works", {
                class = "simpleError", regexp = "`conn` could not be parsed!")
 
 
-  conn <- expect_no_condition(parse_diseasyconn(valid_dbi_conn, type = "target_conn"))
+  expect_no_condition(conn <- parse_diseasyconn(valid_dbi_conn, type = "target_conn"))
   expect_identical(conn, valid_dbi_conn)
-  checkmate::expect_class(conn, "DBIConnection")
 
 
   expect_error(parse_diseasyconn(valid_str_conn, type = "target_conn"),
                class = "simpleError", regexp = "`conn` could not be parsed!")
 
 
-  conn <- expect_no_condition(parse_diseasyconn(null_conn, type = "target_conn"))
+  expect_no_condition(conn <- parse_diseasyconn(null_conn, type = "target_conn"))
   expect_null(conn)
-
-  # Test clean up
-  DBI::dbDisconnect(valid_dbi_conn)
 })
