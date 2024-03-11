9c87b8c25ee0b9184cebe51729d8a7b3ebae633e
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-03-11 13:21:28 +0100

    fix(DiseasystoreBase): Use Id to resolve ambiguities in table locations

--- R/DiseasystoreBase.R
+++ R/DiseasystoreBase.R
@@ -131,13 +131,18 @@ DiseasystoreBase <- R6::R6Class(                                                
       feature_loader <- purrr::pluck(ds_map, feature)
 
       # Determine where these features are stored
-      target_table <- SCDB::id(paste(self %.% target_schema, feature_loader, sep = "."), self %.% target_conn)
-      target_table <- paste(
-        c(purrr::pluck(target_table, "name", "schema"),
-          purrr::pluck(target_table, "name", "table")
-        ),
-        collapse = "."
-      )
+      target_table_id <- SCDB::id(paste(self %.% target_schema, feature_loader, sep = "."), self %.% target_conn)
+
+      if (packageVersion("SCDB") <= "0.3") {
+        target_table <- paste(
+          c(purrr::pluck(target_table_id, "name", "schema"),
+            purrr::pluck(target_table_id, "name", "table")
+          ),
+          collapse = "."
+        )
+      } else {
+        target_table <- target_table_id # From SCDB 0.4 we can use the Id everywhere
+      }
 
 
       # Create log table
@@ -202,8 +207,7 @@ DiseasystoreBase <- R6::R6Class(                                                
 
           # Add the existing computed data for given slice_ts
           if (SCDB::table_exists(self %.% target_conn, target_table)) {
-            ds_existing <- dplyr::tbl(self %.% target_conn, SCDB::id(target_table, self %.% target_conn),
-                                      check_from = FALSE)
+            ds_existing <- dplyr::tbl(self %.% target_conn, target_table_id, check_from = FALSE)
 
             if (SCDB::is.historical(ds_existing)) {
               ds_existing <- ds_existing |>
@@ -233,7 +237,7 @@ DiseasystoreBase <- R6::R6Class(                                                
           SCDB::update_snapshot(
             .data = ds_updated_feature,
             conn = self %.% target_conn,
-            db_table = target_table,
+            db_table = target_table_id,
             timestamp = slice_ts,
             message = glue::glue("ds-range: {start_date} - {end_date}"),
             logger = logger,
@@ -253,7 +257,7 @@ DiseasystoreBase <- R6::R6Class(                                                
 
       # Finally, return the data to the user
       out <- do.call(what = purrr::pluck(private, feature_loader) %.% get,
-                     args = list(target_table = target_table,
+                     args = list(target_table = target_table_id,
                                  slice_ts = slice_ts, target_conn = self %.% target_conn))
 
       # We need to slice to the period of interest.
@@ -603,7 +607,6 @@ DiseasystoreBase <- R6::R6Class(                                                
     # @return (`tibble`)\cr
     #   A data frame containing continuous un-computed date-ranges
     #' @importFrom zoo as.Date
-    #' @importFrom SCDB as.character
     determine_new_ranges = function(target_table, start_date, end_date, slice_ts) {
 
       if (inherits(target_table, "Id")) {
