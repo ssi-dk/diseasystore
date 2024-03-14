58937d0fd6383e5de828b4045a08ec08b15ef80b
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-03-14 16:18:31 +0100

    fix(EcdcRespiratoryViruses): Filter output to study dates

--- tests/testthat/test-DiseasystoreEcdcRespitoryViruses.R
+++ tests/testthat/test-DiseasystoreEcdcRespitoryViruses.R
@@ -9,6 +9,6 @@ test_diseasystore(
   conn_generator = get_test_conns,
   data_files = ecdc_files,
   target_schema = target_schema_1,
-  test_start_date = as.Date("2023-10-01"),
+  test_start_date = as.Date("2022-06-20"),
   slice_ts = "2023-11-24"
 )
