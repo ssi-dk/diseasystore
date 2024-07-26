5d6b8d694ce401204251369734191e8f7496523c
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-07-26 08:44:02 +0200

    fix(simulist_data): Adapt to simulist version 0.3

--- data-raw/simulist_data.R
+++ data-raw/simulist_data.R
@@ -10,8 +10,8 @@ if (rlang::is_installed("epiparameter") && rlang::is_installed("simulist") && rl
     prob_distribution_params = c(mean = 2)
   )
 
-  # Create COVID-19 contact interval
-  contact_interval <- epiparameter::epidist(
+  # Create COVID-19 infectious period
+  infectious_period <- epiparameter::epidist(
     disease = "COVID-19",
     epi_dist = "contact interval",
     prob_distribution = "gamma",
@@ -36,25 +36,48 @@ if (rlang::is_installed("epiparameter") && rlang::is_installed("simulist") && rl
   set.seed(1)
   simulist_data <- simulist::sim_linelist(
     contact_distribution = contact_distribution,
-    contact_interval = contact_interval,
+    infectious_period = infectious_period,
     prob_infect = 0.5,
     onset_to_hosp = onset_to_hosp,
     onset_to_death = onset_to_death,
+    onset_to_recovery = function(x) stats::rnorm(n = x, mean = 2.5, sd = 0.5),
     hosp_risk = 0.2,
     outbreak_start_date = as.Date("2019-12-01"),
-    min_outbreak_size = 1000,
-    add_names = FALSE,
-    add_ct = FALSE
+    outbreak_size = c(1000, 1e9),
+    anonymise = TRUE
   )
 
   # Convert to tibble
   simulist_data <- tibble::as_tibble(simulist_data)
 
   # Convert dates from Date to Date (sic!)
-  # ... Somehow R has stored extra information in the dates that makes them unique despite being the same date
+  # ... Somehow R has stored extra information in the dates that makes them different despite being the same date
   simulist_data <- simulist_data |>
     dplyr::mutate(dplyr::across(tidyselect::starts_with("date_"), ~ as.Date(as.character(.))))
 
-  # Store the simulated linelist
+  # Generate a "random" birth date for each individual
+  simulist_data <- simulist_data |>
+    dplyr::mutate("birth" = .data$date_onset - .data$id %% 365 - .data$age * 365, .before = "age")
+
+  # Convert "outcome" to death and recovery
+  simulist_data <- simulist_data |>
+    dplyr::mutate(
+      "date_discharge" = dplyr::case_when(
+        .data$outcome == "recovered" & !is.na(.data$date_admission) ~ pmax(.data$date_outcome, .data$date_admission),
+        .data$outcome == "died" ~ .data$date_outcome,
+        TRUE ~ NA
+      ),
+      "date_death" = dplyr::case_when(
+        .data$outcome == "died" ~ .data$date_outcome,
+        TRUE ~ NA
+      )
+    ) |>
+    dplyr::select(!dplyr::any_of(c("outcome", "date_outcome")))
+
+  # Remove surplus information
+  simulist_data <- simulist_data |>
+    dplyr::select(!dplyr::any_of(c("case_name", "date_first_contact", "date_last_contact", "ct_value")))
+
+  # Store the simulated line list
   usethis::use_data(simulist_data, overwrite = TRUE)
 }
