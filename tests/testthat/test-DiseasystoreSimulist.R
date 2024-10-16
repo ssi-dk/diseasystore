883968231e9cc33b29477029da6fc886876e0b2b
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-10-16 14:03:57 +0200

    feat(DiseasystoreSimulist): Drop support for SQLite

--- tests/testthat/test-DiseasystoreSimulist.R
+++ tests/testthat/test-DiseasystoreSimulist.R
@@ -4,5 +4,9 @@ test_diseasystore(
   conn_generator = get_test_conns,
   data_files = NULL,
   target_schema = target_schema_1,
-  test_start_date = as.Date("2020-02-20")
+  test_start_date = as.Date("2020-02-20"),
+  # SQLite does not have sufficient date support and dbplyr has inconsistent conversion of dates for SQLite.
+  # Accounting for all of these issues defeats the purpose of `DiseasystoreSimulist` being a example of how to build
+  # a diseasystore
+  skip_feature_tests_on = "SQLiteConnection"
 )
